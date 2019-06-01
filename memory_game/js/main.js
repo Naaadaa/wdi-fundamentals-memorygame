eafc6aad3d3461dea534f4becdb838f4f4c72bb8
nadaalotaibi <naaadaa_tb@outlook.com>
2019-06-01 06:52:26 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,65 @@
+
+//alert('Hello, friends.'); 
+
+var cards = [
+{
+rank :"queen",
+suit:"hearts",
+cardImage:"images/queen-of-hearts.png"
+},
+{
+rank :"queen",
+suit:"diamonds",
+cardImage:"images/queen-of-diamonds.png"
+},
+{
+rank :"king",
+suit:"hearts",
+cardImage:"images/king-of-hearts.png"
+},
+{
+rank :"king",
+suit:"diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+];
+
+var cardsInPlay= []; 
+
+function checkForMatch (){
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+};
+
+function flipCard(cardId)   {	
+
+/*if (cardsInPlay[0] === cardsInPlay[1] ) {
+	alert("You found a match!"); 
+}
+	else { alert ("Sorry, try again") 
+} */
+
+console.log("User flipped " + cards[cardId].rank );
+console.log("User flipped " + cards[cardId].cardImage );  
+console.log("User flipped " + cards[cardId].suit ); 	     
+cardsInPlay.push(cardId.rank);
+checkForMatch(); 
+
+
+
+}; 
+
+
+
+flipCard(0); 
+flipCard(2);
+
+
+
+
+
+
